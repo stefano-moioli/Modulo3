aa8304efa4d65b62880d23c287117085c0cf70f4
stefano-moioli <stefano.moioli10@gmail.com>
2024-02-26 15:48:07 +0100

    constFixed

--- W4D4/js/script.js
+++ W4D4/js/script.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var sections = document.querySelectorAll('.fade-in-element');
+    const sections = document.querySelectorAll('.fade-in-element');
     function isSectionVisible(section) {
-        var elementPosition = section.getBoundingClientRect();
+        const elementPosition = section.getBoundingClientRect();
         return elementPosition.top >= 0 && elementPosition.bottom <= window.innerHeight;
     }
 
     function handleSectionVisibility() {
         sections.forEach(function (section) {
-            var shouldFadeIn = isSectionVisible(section);
+            const shouldFadeIn = isSectionVisible(section);
 
             if (shouldFadeIn) {
                 section.classList.add('fade-in');
@@ -20,6 +20,6 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener('scroll', function () {
         handleSectionVisibility();
     });
-    
+
     handleSectionVisibility();
 });
